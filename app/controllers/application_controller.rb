ba28d0288be5d5f537ebdcacc08f1d76b430c3bc
Eric Furrevig <efurrevig@gmail.com>
2023-02-07 21:41:58 -0500

    some basic nav stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    respond_to :html, :json
 
     protected
 
