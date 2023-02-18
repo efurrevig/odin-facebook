719683e65c48ca2247fed15129aebe338ee7d8ae
Eric Furrevig <efurrevig@gmail.com>
2023-02-17 23:02:42 -0500

    some friend request test changes

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -2,8 +2,19 @@ class FriendRequestsController < ApplicationController
 
     def create
         @friend_request = FriendRequest.new(friend_request_params)
-        @friend_request.save
+        @user = User.find(params[:user_id])
+      
+        respond_to do |format|
+          if @friend_request.save
+            format.html { render json: { success: true, notice: 'Friend request sent.' } }
+            format.json { render json: { success: true, notice: 'Friend request sent.' } }
+          else
+            format.html { render json: { success: false, alert: @friend_request.errors.full_messages } }
+            format.json { render json: { success: false, alert: @friend_request.errors.full_messages } }
+          end
+        end
     end
+
     private
         def friend_request_params
             params.require(:friend_request).permit(:sender_id, :recipient_id)
--- test/controllers/friend_requests_controller_test.rb
+++ test/controllers/friend_requests_controller_test.rb
@@ -11,7 +11,23 @@ class FriendRequestsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create friend request" do
-    post user_friend_requests_url(user_id: @user1.id, friend_request: { sender_id: @user1.id, recipient_id: @user2.id })
-    assert_response :success
+    assert_difference('FriendRequest.count') do
+      post user_friend_requests_url(user_id: @user1.id, 
+                                    friend_request: { sender_id: @user1.id, recipient_id: @user2.id })
+    end
+  end
+
+  test "should not create friend request if friend request already exists" do
+    assert_no_difference('FriendRequest.count') do
+      post user_friend_requests_url(user_id: @user1.id,
+                                    friend_request: { sender_id: 200, recipient_id: 201 })
+    end
+  end
+
+  test "should redirect to friend request index if html request" do
+    post user_friend_requests_url(user_id: @user1.id, 
+                                      friend_request: { sender_id: @user1.id, recipient_id: @user2.id }), xhr: false
+    assert_response :redirect
+    assert_redirected_to user_friend_requests_url(user_id: @user1.id)
   end
 end
