0adbd8e4da8eef9fa5c8c945cef061ab7910082f
Eric Furrevig <efurrevig@gmail.com>
2023-02-21 18:36:43 -0500

    friend request button js

--- app/javascript/controllers/requests_controller.js
+++ app/javascript/controllers/requests_controller.js
@@ -1,17 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+    static targets = [ "acceptFriend", "declineFriend", "check", "x" ]
 
     connect() {
         console.log("RequestsController connected")
     }
 
     accept() {
-        console.log("accept")
+        this.acceptFriendTarget.classList.add("hidden")
+        this.declineFriendTarget.classList.add("hidden")
+        this.checkTarget.classList.remove("hidden")
     }
 
     decline() {
-        console.log("decline")
+        this.acceptFriendTarget.classList.add("hidden")
+        this.declineFriendTarget.classList.add("hidden")
+        this.xTarget.classList.remove("hidden")
     }
 
+
+    /* TODO:
+            Transition to "accepted" or "declined" state
+    */
 }
