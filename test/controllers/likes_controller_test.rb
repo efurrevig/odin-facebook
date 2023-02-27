6de594aad795c607580d90f9aaf96f49c614f94d
Eric Furrevig <efurrevig@gmail.com>
2023-02-27 14:21:27 -0500

    More tests for Likes_controller

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -60,46 +60,70 @@ class LikesControllerTest < ActionDispatch::IntegrationTest
     end
 
     # Post like/dislike_count tests
-    test "should update like_count for post when like is created" do
+    test "should update like_count for post when like is created as true" do
+        assert_difference "Post.find(@post.id).like_count", 1 do
+            post post_likes_path(@post), params: { like: { status: true, user_id: @user1.id }, user: @user1 }
+        end
     end
     
-    test "should update like_count for post when like is updated to true" do
-    end
-
-    test "should update like_count for post when like is updated to false" do
-    end
-
-    test "should update dislike_count for post when like is updated to true" do
-    end
-
-    test "should update dislike_count for post when like is updated to false" do
+    test "should update dislike_count for post when like is created as false" do
+        assert_difference "Post.find(@post.id).dislike_count", 1 do
+            post post_likes_path(@post), params: { like: { status: false, user_id: @user1.id }, user: @user1 }
+        end
     end
 
     test "should update both dislike_count and like_count for post when like is updated to true" do
+        assert_difference({'Post.find(@post2.id).like_count' => 1, 'Post.find(@post2.id).dislike_count' => -1}) do
+            patch post_like_path(@post2, @like2), params: { like: { status: true, user_id: @user1.id }, user_id: @user1.id }
+        end
     end
 
     test "should update both dislike_count and like_count for post when like is updated to false" do
+        assert_difference({'Post.find(@post2.id).like_count' => -1, 'Post.find(@post2.id).dislike_count' => 1}) do
+            patch post_like_path(@post2, @like3), params: { like: { status: false, user_id: @user1.id }, user_id: @user1.id }
+        end
     end
 
     # Comment like/dislike_count tests
     test "should update like_count for comment when like is created" do
+        assert_difference "Comment.find(@comment.id).like_count", 1 do
+            post post_comment_likes_path(@post, @comment), params: { like: { status: true, user_id: @user1.id }, user_id: @user1.id }
+        end
     end
     
     test "should update like_count for comment when like is updated to true" do
+        assert_difference "Comment.find(@comment2.id).like_count", 1 do
+            patch post_comment_like_path(@post, @comment2, @like5), params: { like: { status: true, user_id: @user1.id }, user_id: @user1.id }
+        end
     end
 
     test "should update like_count for comment when like is updated to false" do
+        assert_difference "Comment.find(@comment2.id).like_count", -1 do
+            patch post_comment_like_path(@post, @comment2, @like4), params: { like: { status: false, user_id: @user1.id }, user_id: @user1.id }
+        end
     end
 
     test "should update dislike_count for comment when like is updated to true" do
+        assert_difference "Comment.find(@comment2.id).dislike_count", -1 do
+            patch post_comment_like_path(@post, @comment2, @like5), params: { like: { status: true, user_id: @user1.id }, user_id: @user1.id }
+        end
     end
 
     test "should update dislike_count for comment when like is updated to false" do
+        assert_difference "Comment.find(@comment2.id).dislike_count", 1 do
+            patch post_comment_like_path(@post, @comment2, @like4), params: { like: { status: false, user_id: @user1.id }, user_id: @user1.id }
+        end
     end
 
     test "should update both dislike_count and like_count for comment when like is updated to true" do
+        assert_difference({'Comment.find(@comment2.id).like_count' => 1, 'Comment.find(@comment2.id).dislike_count' => -1}) do
+            patch post_comment_like_path(@post, @comment2, @like5), params: { like: { status: true, user_id: @user1.id }, user_id: @user1.id }
+        end
     end
 
     test "should update both dislike_count and like_count for comment when like is updated to false" do
+        assert_difference({'Comment.find(@comment2.id).like_count' => -1, 'Comment.find(@comment2.id).dislike_count' => 1}) do
+            patch post_comment_like_path(@post, @comment2, @like4), params: { like: { status: false, user_id: @user1.id }, user_id: @user1.id }
+        end
     end
 end
